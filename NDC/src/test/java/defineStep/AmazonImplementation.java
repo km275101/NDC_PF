7ddd85f418cdd2f155780baea8608b0539233867
Kame <Kame@192.168.2.11>
2019-05-07 23:47:16 +0530

    update

--- NDC/src/test/java/defineStep/AmazonImplementation.java
+++ NDC/src/test/java/defineStep/AmazonImplementation.java
@@ -1,23 +1,15 @@
 package defineStep;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
 import coreAction.Initialize;
-import coreAction.OpenAndCloseBrowser;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pageElements.LoginTestPE;
+import pageElements.LoginTestPE2;
+import pageFunctions.LoginTestPF;
 
 public class AmazonImplementation extends Initialize{
+	
 	@Given("^users open login page$")
 	public void users_open_login_page() throws Throwable {
 		System.out.println("Opening amazon URL");
@@ -26,19 +18,26 @@ public class AmazonImplementation extends Initialize{
 
 	@When("^user enters credentials$")
 	public void user_enters_credentials() throws Throwable {
-		LoginTestPE lpe = new LoginTestPE(driver);
-		lpe.clickYourOrders();
+		lpf = new LoginTestPF(driver);
+		
+		lpf.clickYourOrders();
+		lpf.setUsername("km275101@gmail.com");
+		lpf.clickLogin();
+		lpf.setPass("kame9454");
+		
 	}
 
 	@And("^click submit button$")
 	public void click_submit_button() throws Throwable {
-		//loginTest.clickLogin();
+		lpe2 = new LoginTestPE2(driver);
+		lpe2.submit.click();
+		
 	}
 
 	@Then("^user sees the login page$")
 	public void user_sees_the_login_page() throws Throwable {
 	    System.out.println("Successful login");
-	   // closeBrowser();
+	   closeBrowser();
 	}
 
 }
