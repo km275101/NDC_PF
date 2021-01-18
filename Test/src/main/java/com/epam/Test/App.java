94a01c9b80bc72affff9676673735a59e2e0db88
kamlesh_maurya <kamlesh_maurya@epam.com>
2021-01-18 11:07:19 +0530

    Adding Test Project

---
+++ Test/src/main/java/com/epam/Test/App.java
@@ -0,0 +1,13 @@
+package com.epam.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
